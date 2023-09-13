7fc36ef99d4af6401ef6d04a6d0dd6b8653e9eda
abdullahalsazib <mdsazibhossin2021@gmail.com>
2023-09-14 04:12:08 +0600

    add all

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-500">
+        <h1 className=" text-center text-[50px] capitalize font-bold">
+          hello world
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
