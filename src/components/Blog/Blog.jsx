8e750ded9c458497ce4c73e514d5d5bbf18f1d9c
Abdullah Al Sazib <99520600+Abdullahalsazib@users.noreply.github.com>
2023-09-14 17:06:37 +0000

    Update Blog.jsx

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div>
+      <div>
+        <h1 className=" bg-yellow-300">hello</h1>
+      </div>
+    </div>
+  );
 }
